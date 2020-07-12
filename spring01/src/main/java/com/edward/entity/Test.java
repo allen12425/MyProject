7b23cf9e922de8388f3be090670bf3c745b78b86
allen12425 <1242570922@qq.com>
2020-07-12 13:09:30 +0800

    fasf

---
+++ spring01/src/main/java/com/edward/entity/Test.java
@@ -0,0 +1,9 @@
+package com.edward.entity;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String name = "'or 'xx'= 'xx' or'";
+        System.out.println(name);
+    }
+}
